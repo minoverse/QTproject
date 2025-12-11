b564d606238a2eab383d86d7350b3d66e3d5ac3a
minoverse <121744450+minoverse@users.noreply.github.com>
2025-12-11 22:34:34 +0100

    Resize main window and update output view dimensions

--- ButtonNetowrk/main.cpp
+++ ButtonNetowrk/main.cpp
@@ -17,23 +17,28 @@ int main(int argc, char *argv[]) {
 
     QWidget* mainWindow = new QWidget;
     mainWindow->setWindowTitle("Hopfield Fractional Network");
-    mainWindow->resize(1200, 600);
+    mainWindow->resize(1500, 800);   // larger window
 
     QSplitter* splitter = new QSplitter(Qt::Horizontal, mainWindow);
     ButtonNetwork* builder = new ButtonNetwork;
 
     QTextEdit* outputView = new QTextEdit;
     outputView->setReadOnly(true);
-    outputView->setMinimumWidth(500);
+    outputView->setMinimumWidth(700);     // larger right pane
     builder->updateEquationEditor(outputView);
 
     splitter->addWidget(builder);
     splitter->addWidget(outputView);
 
-    QPushButton* clearBtn = new QPushButton("Clear");
+    // Give more width to the right by default
+    QList<int> sizes;
+    sizes << 600 << 900;
+    splitter->setSizes(sizes);
+
+    QPushButton* clearBtn   = new QPushButton("Clear");
     QPushButton* computeBtn = new QPushButton("Compute");
-    QPushButton* graphBtn = new QPushButton("Show Graph");
-    QPushButton* tableBtn = new QPushButton("Show Output Table");
+    QPushButton* graphBtn   = new QPushButton("Show Graph");
+    QPushButton* tableBtn   = new QPushButton("Show Output Table");
 
     QComboBox* solverMode = new QComboBox;
     solverMode->addItem("ODE");
@@ -61,14 +66,16 @@ int main(int argc, char *argv[]) {
     layout->addWidget(splitter);
     layout->addLayout(buttonLayout);
 
-    QObject::connect(clearBtn, &QPushButton::clicked, builder, &ButtonNetwork::clearNetwork);
-    QObject::connect(computeBtn, &QPushButton::clicked, builder, &ButtonNetwork::computeResults);
-    QObject::connect(graphBtn, &QPushButton::clicked, builder, &ButtonNetwork::showGraph);
-    QObject::connect(tableBtn, &QPushButton::clicked, builder, &ButtonNetwork::showTable);
-    QObject::connect(solverMode, &QComboBox::currentTextChanged, builder, &ButtonNetwork::setSolverMode);
-    QObject::connect(tmaxInput, qOverload<int>(&QSpinBox::valueChanged), builder, &ButtonNetwork::setTimeLimit);
+    QObject::connect(clearBtn,  &QPushButton::clicked, builder, &ButtonNetwork::clearNetwork);
+    QObject::connect(computeBtn,&QPushButton::clicked, builder, &ButtonNetwork::computeResults);
+    QObject::connect(graphBtn,  &QPushButton::clicked, builder, &ButtonNetwork::showGraph);
+    QObject::connect(tableBtn,  &QPushButton::clicked, builder, &ButtonNetwork::showTable);
+    QObject::connect(solverMode,&QComboBox::currentTextChanged, builder, &ButtonNetwork::setSolverMode);
+    QObject::connect(tmaxInput, qOverload<int>(&QSpinBox::valueChanged),
+                     builder, &ButtonNetwork::setTimeLimit);
 
-    QObject::connect(builder, &ButtonNetwork::fileSaved, [=](const QString& path) {
+    QObject::connect(builder, &ButtonNetwork::fileSaved,
+                     [=](const QString& path) {
         QFile file(path);
         if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
             QTextStream in(&file);
